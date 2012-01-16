9c9e9a4ffd86f5e9e2c48ce6008415ccd69df6df
Wang Lun <iseuer@gmail.com>
2012-01-16 10:54:52 +0800

    postinst palm-package不会打包postinst

--- linux/package-it.sh
+++ linux/package-it.sh
@@ -7,4 +7,4 @@ rsync -r --exclude=.DS_Store --exclude=.svn ../enyo/ STAGING
 ./buildit_for_device.sh $1
 cp Build_Device/xwtweak_plugin STAGING
 echo "filemode.755=xwtweak_plugin" > STAGING/package.properties
-palm-package STAGING
+#palm-package STAGING
